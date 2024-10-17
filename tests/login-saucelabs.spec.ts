a8c759f0de38e3aebead022ce9657c899539068e
Jose Colina <jose.colina.eng@gmail.com>
2024-10-17 09:45:51 -0400

    add allure

--- tests/login-saucelabs.spec.ts
+++ tests/login-saucelabs.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from '../support/fixtures'
-
-test('should login with valid credentials', async ({ loginPage, inventoryPage }) => {
-    await loginPage.visit();
-    await loginPage.login('standard_user', 'secret_sauce');
-
-    await expect(inventoryPage.invetoryContainer).toBeVisible()
+test.describe('Login', () => {
+    test('should login with valid credentials', async ({ loginPage, inventoryPage }) => {
+        await loginPage.visit();
+        await loginPage.login('standard_user', 'secret_sauce');
+    
+        await expect(inventoryPage.invetoryContainer).toBeVisible()
+    })
 })
